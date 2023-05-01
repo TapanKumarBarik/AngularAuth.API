ade26434ccb2bf1bb4b25362ae5f88b42b34e934
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2023-05-01 18:35:26 +0530

    Add project files.

---
+++ AngularAuth.API/Model/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AngularAuth.API.Model
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public string Token { get; set; }
+    }
+}
